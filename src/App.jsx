ec3a0f6c4d77eecb96b8bae265e1b7020cfb97c0
TCSon03 <truongcongsson@gmail.com>
2025-05-19 09:41:28 +0700

    setup react vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='container'>
+      <h1>Chào Mừng Bạn Đến Với Lớp Học Của Sơn</h1>
+    </div>
+  )
+}
+
+export default App

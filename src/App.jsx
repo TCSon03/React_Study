51d3be2b2a4584bf8d6e1137c1f8805c8bfee5bf
TCSon03 <truongcongsson@gmail.com>
2025-05-19 10:50:46 +0700

    Tìm hiểu co bản về UseState

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  // Nut tang 1
+  const handleTang = () => {
+    setCount((prev) => {
+      return prev + 1;
+    });
+  };
+  // Nut giam
+  const handleGiam = () => {
+    setCount((prev) => {
+      if (prev > 0) return prev - 1;
+      return prev;
+    });
+  };
+
+  // Tăng 5 lần
+  const handleTang5 = () => {
+    for (let i = 0; i < 5; i++) {
+      setCount((prev) => prev + 1);
+    }
+  };
+
   return (
-    <div className='container'>
-      <h1>Chào Mừng Bạn Đến Với Lớp Học Của Sơn</h1>
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={handleTang}>Tăng</button>
+      <button onClick={handleGiam}>Giam</button>
+      <button onClick={handleTang5}>Tăng + 5</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

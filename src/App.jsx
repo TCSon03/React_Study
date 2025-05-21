20a1e2234e5ec397375f214673e7fc27d922f10c
TCSon03 <truongcongsson@gmail.com>
2025-05-21 21:04:43 +0700

    cấu hình cơ bản

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import './App.css'
 
 function App() {
   return (
     <div className='container'>
-      <h1>Chào Mừng Bạn Đến Với Lớp Học Của Sơn</h1>
+      <h1>Nhìn cái chó j</h1>
     </div>
   )
 }
